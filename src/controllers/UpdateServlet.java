9526e1108688e73c9919250a9bed16587f36e9e6
nakoto <nakoto15021@yahoo.co.jp>
2020-03-24 22:53:35 +0900

    Add database files

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -1,5 +1,4 @@
 package controllers;
-
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.List;
@@ -15,61 +14,41 @@ import javax.servlet.http.HttpServletResponse;
 import models.Task;
 import models.validators.TaskListValidator;
 import utils.DBUtil;
-
 @WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
     public UpdateServlet() {
         super();
-
     }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String _token = (String) request.getParameter("_token");
         if (_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
-
             Task m = em.find(Task.class, (Integer) (request.getSession().getAttribute("message_id")));
-
             String title = request.getParameter("title");
             m.setTitle(title);
-
             String content = request.getParameter("content");
             m.setContent(content);
-
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             m.setUpdated_at(currentTime);
-
             List<String> errors = TaskListValidator.validate(m);
             if (errors.size() > 0) {
                 em.close();
-
                 request.setAttribute("_token", request.getSession().getId());
                 request.setAttribute("message", m);
                 request.setAttribute("errors", errors);
-
                 RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
                 rd.forward(request, response);
             } else {
-
                 em.getTransaction().begin();
                 em.getTransaction().commit();
                 request.getSession().setAttribute("flush", "更新が完了しました。");
                 em.close();
-
                 request.getSession().removeAttribute("message_id");
                 response.sendRedirect(request.getContextPath() + "/index");
             }
 
-            em.getTransaction().begin();
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "更新が完了しました。");
-            em.close();
-
-            request.getSession().removeAttribute("message_id");
-            response.sendRedirect(request.getContextPath() + "/index");
         }
     }
 }
